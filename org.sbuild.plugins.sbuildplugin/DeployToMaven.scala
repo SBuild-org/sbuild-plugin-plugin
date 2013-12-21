5a4907db41c781e213fad0700310357459b11c84
Tobias Roeser <le.petit.fou@web.de>
2013-12-21 18:37:53 +0100

    Updated release script.

--- org.sbuild.plugins.sbuildplugin/DeployToMaven.scala
+++ org.sbuild.plugins.sbuildplugin/DeployToMaven.scala
@@ -1,6 +1,6 @@
 import de.tototec.sbuild._
 
-@version("0.7.0")
+@version("0.7.1")
 @classpath(
   "mvn:org.apache.ant:ant:1.8.4",
   "../../maven-deploy/org.sbuild.plugins.mavendeploy/target/org.sbuild.plugins.mavendeploy-0.0.9000.jar"
@@ -8,32 +8,29 @@ import de.tototec.sbuild._
 class SBuild(implicit _project: Project) {
 
   val namespace = "org.sbuild.plugins.sbuildplugin"
-  val version = "0.2.0"
+  val version = "0.2.1"
+  val url = "https://github.com/SBuild-org/sbuild-plugin-plugin"
 
   import org.sbuild.plugins.mavendeploy._
 
-  Plugin[MavenDeploy] configure { p =>
-    p.groupId = "org.sbuild"
-    p.artifactId = namespace
-    p.version = version
-    p.artifactName = Some("SBuild SBuild Plugin")
-    p.description = Some("An SBuild Plugin to easily create SBuild plugins.")
-
-    p.repository = Repository.SonatypeOss
-    val scm = "https://github.com/SBuild-org/sbuild-plugin-plugin"
-    p.scm = Option(Scm(url = scm, connection = scm))
-    p.developers = Seq(Developer(id = "TobiasRoeser", name = "Tobias Roeser", email = "le.petit.fou@web.de"))
-    p.licenses = Seq(License.Apache20)
-    p.gpg = true
-    p.licenses = Seq(License.Apache20)
-    p.url = Some("https://github.com/SBuild-org/sbuild-plugin-plugin")
-
-    p.files = Map(
+  Plugin[MavenDeploy] configure { _.copy(
+    groupId = "org.sbuild",
+    artifactId = namespace,
+    version = version,
+    artifactName = Some("SBuild SBuild Plugin"),
+    description = Some("An SBuild Plugin to easily create SBuild plugins."),
+    repository = Repository.SonatypeOss,
+    scm = Option(Scm(url = url, connection = url)),
+    developers = Seq(Developer(id = "TobiasRoeser", name = "Tobias Roeser", email = "le.petit.fou@web.de")),
+    gpg = true,
+    licenses = Seq(License.Apache20),
+    url = Some(url),
+    files = Map(
       "jar" -> s"target/${namespace}-${version}.jar",
       "sources" -> s"target/${namespace}-${version}-sources.jar",
       "javadoc" -> "target/fake.jar"
     )
-  }
+  )}
 
   Target("target/fake.jar") dependsOn "LICENSE.txt" exec { ctx: TargetContext =>
     import de.tototec.sbuild.ant._
